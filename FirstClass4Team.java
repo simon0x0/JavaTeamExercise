581f2c241c66d34f4b33698b93edb4dfc4904947
simon0x0 <70833874+simon0x0@users.noreply.github.com>
2025-04-24 20:14:28 +0800

    feat: first version-print the first member name

---
+++ FirstClass4Team.java
@@ -0,0 +1,7 @@
+public class FirstClass4Team {
+    public static void main(String[] args){
+        System.out.println("這是用來練習Github協作的程式！");
+        System.out.println("\"學號：\"01257108\"\n姓名：\"許景皓\"");
+    }
+
+}
