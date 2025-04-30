5cb9accf541b41dfac13adfcd71ce4e2d9b046e8
Ke94 <candy79449@gmail.com>
2025-04-30 13:23:06 +0800

    Update FirstClass4Team.java
    feat: second version-print the second member name

--- FirstClass4Team.java
+++ FirstClass4Team.java
@@ -2,6 +2,7 @@ public class FirstClass4Team {
     public static void main(String[] args){
         System.out.println("這是用來練習Github協作的程式！");
         System.out.println("\"學號：\"01257108\"\n姓名：\"許景皓\"");
+        System.out.println("\"學號：\"01257120\"\n姓名：\"柯辰翰\"");
     }
 
 }
