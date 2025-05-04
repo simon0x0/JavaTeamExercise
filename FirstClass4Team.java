a95ef216688baa2cd1c5d7b5a5c0b2b7cc106020
36760230 <baby36760230@gmail.com>
2025-05-04 20:48:35 +0800

    Update FirstClass4Team.java

--- FirstClass4Team.java
+++ FirstClass4Team.java
@@ -3,7 +3,7 @@ public class FirstClass4Team {
         System.out.println("這是用來練習Github協作的程式！");
         System.out.println("\"學號：\"01257108\"\n姓名：\"許景皓\"");
         System.out.println("\"學號：\"01257120\"\n姓名：\"柯辰翰\"");
-        System.out.println("\"學號：\"01257123\"\n姓名：\"高翊誠\"");;
+        System.out.println("\"學號：\"01257123\"\n姓名：\"高翊誠\"");
     }
 
 }
